80b2052c7dc1f6680dca9c35c60a0425d5244e96
Meanz12 <meanz1331@gmail.com>
2022-11-12 20:31:20 +0900

    View

--- src/main/java/com/websocket/chat/controller/ChatController.java
+++ src/main/java/com/websocket/chat/controller/ChatController.java
@@ -1,24 +1,28 @@
 package com.websocket.chat.controller;
 
 import com.websocket.chat.dto.ChatMessage;
+import com.websocket.chat.pubsub.RedisPublisher;
+import com.websocket.chat.repository.ChatRoomRepository;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
-    public ChatController(SimpMessageSendingOperations messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
+    public ChatController(RedisPublisher redisPublisher,ChatRoomRepository chatRoomRepository) {
+        this.redisPublisher = redisPublisher;
+        this.chatRoomRepository = chatRoomRepository;
     }
 
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
         if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
